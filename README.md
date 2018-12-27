8698ea5bc8f0fb551a130e4fce4c89ea5b43ec21
Mathias Frey <mathiasfrey@georgebanks.local>
2018-12-27 18:34:17 +0100

    VC-65 initiated the go project for vcn command line interface chose a cli parsing library fulfilled the basic interface requirement of vcn verify

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # vcn-cli
+
+GOing to take over the world.
+
+## Development
+
+Clone this directory into your `GOPATH`, usually this is `$HOME/go/src/`
+
+Install vcn system-wide.
+
+```
+$> cd vcn-cli/vcn
+$> go install
+```
---
+++ vcn/vcn.go
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Built on top of CLI (MIT license)
+ * https://github.com/urfave/cli#overview
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "vcn"
+	app.Version = "0.0.1"
+
+	app.Commands = []cli.Command{
+		{
+			Category: "Artifact actions",
+			Name:     "verify",
+			Aliases:  []string{"v"},
+			Usage:    "verify against blockchain",
+			Action: func(c *cli.Context) error {
+				fmt.Println("verified artifact: ", c.Args().First())
+				return nil
+			},
+			Flags: []cli.Flag{
+				cli.BoolFlag{Name: "hash"},
+			},
+		},
+		{
+			Category: "Artifact actions",
+			Name:     "commit",
+			Aliases:  []string{"c"},
+			Usage:    "commit in blockchain",
+			Action: func(c *cli.Context) error {
+				fmt.Println("committed artifact: ", c.Args().First())
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
