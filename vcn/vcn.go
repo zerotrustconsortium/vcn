8e3d349765e37c929e093aac96aaad66b7418bed
moshix <moshix@gmail.com>
2018-12-31 02:43:33 -0600

    Update vcn.go
    Made copyright 2018-2019

--- vcn/vcn.go
+++ vcn/vcn.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 vChain, Inc. All Rights Reserved.
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
  * This software is released under GPL3.
  * The full license information can be found under:
  * https://www.gnu.org/licenses/gpl-3.0.en.html
