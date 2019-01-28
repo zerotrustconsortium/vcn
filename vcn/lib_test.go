816fe89298297c98186fc5c07bb5eaf5a53cbcf3
Mathias Frey <mathiasfrey@georgebanks.local>
2019-01-28 16:03:55 +0100

    VC-65 added test for Jenkins

---
+++ vcn/lib_test.go
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package main
+
+import (
+	"testing"
+)
+
+const expectedHash string = "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b"
+
+func TestHash(t *testing.T) {
+	if hash("tests/testHash.example") != expectedHash {
+		t.Error(`hash("tests/testHash.example") does not match`)
+	}
+
+}
