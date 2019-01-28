d638863f68c1053ddb66ba4a89f55d5cd9ba2f16
Mathias Frey <mathiasfrey@georgebanks.local>
2019-01-28 16:45:31 +0100

    VC-65 test layout according to Andreas

--- vcn/lib_test.go
+++ vcn/lib_test.go
@@ -15,8 +15,8 @@ import (
 const expectedHash string = "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b"
 
 func TestHash(t *testing.T) {
-	if hash("tests/testHash.example") != expectedHash {
-		t.Error(`hash("tests/testHash.example") does not match`)
+	if hash("../resources/testHash.example") != expectedHash {
+		t.Error(`hash("../resources/testHash.example") does not match`)
 	}
 
 }
