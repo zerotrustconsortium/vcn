cee530eb69c9e012d25e634b909a839ff72fc016
Mathias Frey <mathias.c.frey@gmail.com>
2019-02-19 10:47:22 +0100

    VC-206 tagged v.0.1.2

--- vcn/artifact.go
+++ vcn/artifact.go
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
 package main
 
 import (
--- vcn/auth.go
+++ vcn/auth.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
 package main
 
 import (
