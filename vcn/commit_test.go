282305e753d1dff975b9cd285e8a7583f41460c3
Mathias Frey <mathias.c.frey@gmail.com>
2019-02-25 10:39:13 +0100

    VC-215 added end-to-end test dor experimental docker feature

---
+++ vcn/commit_test.go
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package main
+
+/*
+func TestCommitDocker(t *testing.T) {
+
+	// Run the Docker hello world example
+	// this ensures that the image will be present
+	c := exec.Command("docker", "run", helloWorld)
+
+
+	//commitHash(hash string, owner string, passphrase string, filename string)
+
+
+}
+*/
---
+++ vcn/verify_test.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+)
+
+func TestVerifyDocker(t *testing.T) {
+	// make sure the docker verification works e2e
+	const helloWorld string = "hello-world"
+
+	// Run the Docker hello world example
+	// this ensures that the image will be present
+	exec.Command("docker", "run", helloWorld)
+	//fmt.Println(c)
+
+	h := getDockerHash(fmt.Sprintf("docker:%s", helloWorld))
+
+	verified, _, _ := verifyHash(h)
+
+	if verified != false {
+		t.Error(fmt.Sprintf("Could not verify: <docker:%s>", helloWorld))
+	}
+
+}
