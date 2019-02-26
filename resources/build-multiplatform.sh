27951b2dddb46ac04e8df5a8b22f45eac3cba2c3
Andreas Meingast <ameingast@gmail.com>
2019-02-26 13:44:11 +0100

    Modified build-multiplatform.sh to dump binaries into dist folder

--- resources/build-multiplatform.sh
+++ resources/build-multiplatform.sh
@@ -3,4 +3,5 @@
 #env GOOS=windows GOARCH=amd64 go build
 #env GOOS=linux GOARCH=amd64 go build
 read -p 'Version (9.9.9): ' version
-xgo --targets=linux/amd64,windows/amd64,darwin/amd64 -out vcn-v$version ../vcn
+mkdir -p ../dist
+xgo --targets=linux/amd64,windows/amd64,darwin/amd64 -out ../dist/vcn-v$version ../vcn
