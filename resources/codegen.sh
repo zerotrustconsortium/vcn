c0684a596d5b7d674032568a89856b8fdade471f
Andreas Meingast <ameingast@gmail.com>
2019-03-15 18:09:41 +0100

    Removed codegen tools

--- resources/codegen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-docker run -v "$(pwd):/tmp" ethereum/solc:0.4.24 --abi /tmp/resources/Proof.sol -o /tmp
-docker run -v "$(pwd):/tmp" ethereum/client-go:alltools-stable abigen --abi /tmp/Proof.abi --pkg main --type Proof --out /tmp/vcn/proof.go
-rm -f Proof.abi
